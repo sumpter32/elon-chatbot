c42e3adb73345020175ca83d639ec65b46debc54
Steven S <sumpter32@gmail.com>
2023-02-22 09:40:12 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,125 +1,150 @@
-import bot from './assets/srs.png'
-import user from './assets/user.svg'
+import bot from './assets/srs.png';
+import user from './assets/user.svg';
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 const options = ["Option 1", "Option 2"];
-const chatbot_response = await get_response(conversation_history, user_input, options);
 
+async function getBotResponse(conversation_history, user_input, options) {
+  const response = await fetch('https://lincon-chat-bot.onrender.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: conversation_history + "\nUser: " + user_input + "\nChatbot:",
+      options: options
+    })
+  });
 
-let loadInterval
+  if (response.ok) {
+    const data = await response.json();
+    return data.bot.trim(); // trims any trailing spaces/'\n'
+  } else {
+    const err = await response.text();
+    throw new Error(err);
+  }
+}
+
+let loadInterval;
 
 function loader(element) {
-    element.textContent = ''
+  element.textContent = '';
 
-    loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
-        element.textContent += '.';
+  loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += '.';
 
-        // If the loading indicator has reached three dots, reset it
-        if (element.textContent === '....') {
-            element.textContent = '';
-        }
-    }, 300);
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300);
 }
 
 function typeText(element, text) {
-    let index = 0
+  let index = 0;
 
-    let interval = setInterval(() => {
-        if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
-        } else {
-            clearInterval(interval)
-        }
-    }, 19)
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 19);
 }
 
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
 function generateUniqueId() {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
-    return `id-${timestamp}-${hexadecimalString}`;
+  return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
-    return (
-        `
-        <div class="wrapper ${isAi && 'ai'}">
-            <div class="chat">
-                <div class="profile">
-                    <img 
-                      src=${isAi ? bot : user} 
-                      alt="${isAi ? 'bot' : 'user'}" 
-                    />
-                </div>
-                <div class="message" id=${uniqueId}>${value}</div>
-            </div>
-        </div>
+  return (
     `
-    )
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+          <img 
+            src=${isAi ? bot : user} 
+            alt="${isAi ? 'bot' : 'user'}" 
+          />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+  `
+  );
 }
 
 const handleSubmit = async (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const data = new FormData(form)
+  const data = new FormData(form);
 
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
 
-    // to clear the textarea input 
-    form.reset()
+  // to clear the textarea input 
+  form.reset();
 
-    // bot's chatstripe
-    const conversation_history = chatContainer.textContent;
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  // bot's chatstripe
+  const conversation_history = chatContainer.textContent;
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId);
 
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv);
 
-    const response = await fetch('https://lincon-chat-bot.onrender.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: conversation_history + "\nUser: " + data.get('prompt') + "\nChatbot:"
-        })
-    })
+  try {
+    const botResponse = await getBotResponse(conversation_history, data.get('prompt'), options);
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = " ";
+    typeText(messageDiv, botResponse);
+  } catch (err) {
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+};
 
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
 
-    if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+async function get_response(conversation_history, user_input, options) {
+  const response = await fetch('https://lincon-chat-bot.onrender.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: conversation_history + "\nUser: " + user_input + "\nChatbot:",
+      options: options,
+    }),
+  });
 
-        typeText(messageDiv, parsedData)
-    } else {
-        const err = await response.text()
+  if (!response.ok) {
+    throw new Error('Failed to get response from server');
+  }
 
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
-    }
+  const data = await response.json();
+  return data.bot.trim();
 }
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e)
-    }
-})
- 
