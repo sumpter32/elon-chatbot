819e27dbf74bb19a74d4e4a67016e2371b29e094
Steven S <sumpter32@gmail.com>
2023-02-22 09:31:11 +0800

    Update server.js
    testing

--- server/server.js
+++ server/server.js
@@ -11,32 +11,66 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+const model_engine = "text-davinci-003";
+const chatbot_prompt = `pretend you are Jesus Christ from the living bible.
+
+<conversation history>
+
+User: <user input>
+Chatbot:`;
+
+async function get_response(conversation_history, user_input, options) {
+  const option1 = options[0];
+  const option2 = options[1];
+  const prompt = chatbot_prompt.replace(
+    "<conversation_history>", conversation_history)
+    .replace("<user input>", user_input)
+    .replace("<option 1>", option1)
+    .replace("<option 2>", option2);
+
+  // Get the response from GPT-3
+  const response = await openai.createCompletion({
+    model: model_engine,
+    prompt: prompt,
+    max_tokens: 1500,
+    n: 1,
+    stop: null,
+    temperature: 0.8
+  });
+
+  // Extract the response from the response object
+  const response_text = response.data.choices[0].text;
+
+  const chatbot_response = response_text.trim();
+
+  return chatbot_response;
+}
+
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+let conversation_history = "";
+
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from elon!'
+    message: 'Hello from George Carlin'
   })
 })
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const user_input = req.body.prompt;
+    let { conversation_history } = req.body;
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `pretend you are Albert Einstein. ${prompt}`,
-      temperature: 0.8, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.2, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
+    const chatbot_response = await get_response(model_engine, chatbot_prompt, conversation_history, user_input);
+
+    conversation_history += `User: ${user_input}\nChatbot: ${chatbot_response}\n`;
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: chatbot_response,
+      conversation_history
     });
 
   } catch (error) {
@@ -44,5 +78,6 @@ app.post('/', async (req, res) => {
     res.status(500).send(error || 'Something went wrong');
   }
 })
+
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
